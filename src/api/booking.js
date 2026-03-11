2bbf8bae09009dcd31a0cdf1c2d01c4a92448758
neoapplication23-lab <neoapplication23@gmail.com>
2026-03-11 23:44:54 +0100

    Update booking.js

--- src/api/booking.js
+++ src/api/booking.js
@@ -3,5 +3,5 @@ import { apiGet, apiPost } from './client'
 export const getBooking = (code) =>
   apiGet('get_booking', { code })
 
-export const submitOrder = ({ linkId, items, total }) =>
-  apiPost('submit_order', { linkId, items, total })
+export const submitOrder = ({ linkId, items, total, clientDetails, apaInvoice }) =>
+  apiPost('submit_order', { linkId, items, total, clientDetails, apaInvoice })
